29584be4103e08428253fd5e15bc088ce804ed60
Sylvester <violetsmyster@gmail.com>
2023-12-13 02:27:20 +0000

    Improve system data usage

--- Hotspot Meter/App.swift
+++ Hotspot Meter/App.swift
@@ -138,54 +138,6 @@ struct DataUsageInfo {
     }
 }
 
-class SystemDataUsage {
-    private static let wifiInterfacePrefix = "en"
-    
-    class func getDataUsage() -> DataUsageInfo {
-        var ifaddr: UnsafeMutablePointer<ifaddrs>?
-        var dataUsageInfo = DataUsageInfo()
-        
-        guard getifaddrs(&ifaddr) == 0 else { return dataUsageInfo }
-        while let addr = ifaddr {
-            guard let info = getDataUsageInfo(from: addr) else {
-                ifaddr = addr.pointee.ifa_next
-                continue
-            }
-            dataUsageInfo.updateInfoByAdding(info)
-            ifaddr = addr.pointee.ifa_next
-        }
-        
-        freeifaddrs(ifaddr)
-        
-        return dataUsageInfo
-    }
-    
-    private class func getDataUsageInfo(from infoPointer: UnsafeMutablePointer<ifaddrs>) -> DataUsageInfo? {
-        let pointer = infoPointer
-        guard let name = String(cString: pointer.pointee.ifa_name, encoding: .utf8) else { return nil }
-        let addr = pointer.pointee.ifa_addr.pointee
-        guard addr.sa_family == UInt8(AF_LINK) else { return nil }
-        
-        return dataUsageInfo(from: pointer, name: name)
-    }
-    
-    private class func dataUsageInfo(from pointer: UnsafeMutablePointer<ifaddrs>, name: String) -> DataUsageInfo {
-        var networkData: UnsafeMutablePointer<if_data>?
-        var dataUsageInfo = DataUsageInfo()
-        
-        if name.hasPrefix(wifiInterfacePrefix) {
-            networkData = unsafeBitCast(pointer.pointee.ifa_data, to: UnsafeMutablePointer<if_data>.self)
-            if let data = networkData {
-                dataUsageInfo.wifiSent += UInt64(data.pointee.ifi_obytes)
-                dataUsageInfo.wifiReceived += UInt64(data.pointee.ifi_ibytes)
-            }
-            
-        }
-        
-        return dataUsageInfo
-    }
-}
-
 struct TotalData: Codable {
     var sent: UInt64
     var received: UInt64
